e8225294356d4c5fdf9bf9109d8ef9506609ea68
Yifan Yu <yvifan@gmail.com>
2018-05-10 16:59:55 +0300

    Prepare for more layers

--- dcgan.srcs/sim_1/new/top_layer_1_tb.v
+++ dcgan.srcs/sim_1/new/top_layer_1_tb.v
@@ -23,9 +23,35 @@ reg clk, reset;
 reg btn_tick = 1'b0, read_spi_tick = 1'b0;
 
 reg [2:0] state = idle;
+reg [3:0] layer = 4'd0;
+
+// configurations for the first layer
+reg [13:0] m = 14'd1, k = 14'd100, n = 14'd32;
+reg [7:0] rhs_offset = 8'd139;
+reg [9:0] n_output_plane = 10'd2;
+reg [9:0] output_plane_start = 10'd0;
+reg [9:0] output_plane_batch_size = 10'd1;
+reg [6:0] output_h = 7'd4;
+reg [6:0] output_w = 7'd4;
+reg [6:0] input_h = 7'd1;
+reg [6:0] input_w = 7'd1;
+reg [2:0] kernel_h = 3'd4;
+reg [2:0] kernel_w = 3'd4;
+reg [2:0] pad_h = 3'd0;
+reg [2:0] pad_w = 3'd0;
+reg [2:0] stride_h = 3'd1;
+reg [2:0] stride_w = 3'd1;
+reg [2:0] dilation_h = 3'd1;
+reg [2:0] dilation_w = 3'd1;
+reg [31:0] term4 = 32'd1723600;
+reg [31:0] sa_f = 32'h3cebbba5;
+reg [31:0] sb_f = 32'h3b8a47ed;
+reg [15:0] input_total_size = 16'd32;
+reg [15:0] input_plane_size = 16'd16;
+
+// keep track of weight loading
 reg [1:0] load_byte = 2'b00;
 reg [17:0] weight_bytes_loaded = 18'd0;
-reg [9:0] output_plane_start = 10'd0;
 
 // Wishbone inputs
 reg i_wb_cyc = 1'b0, i_wb_data_stb = 1'b0;
@@ -111,26 +137,27 @@ bram #(.ADDR_WIDTH(10), .DATA_WIDTH(32), .DATA_FILE("tc_bias.mem")) bram_tc_bias
 // --- Module instances
 
 col_vec col_vec_unit
-    (.clk(clk), .start_tick(start_tick_cv), .m(14'd1), .k(14'd100), .rhs_offset(8'd139),
+    (.clk(clk), .start_tick(start_tick_cv), .m(m), .k(k), .rhs_offset(rhs_offset),
      .a(input_rd), .a_rd_addr(addr_input_rd_col_vec), .addr_cvw(addr_cv0), .val(cvw), .we(we_cv), .done_tick(done_tick_cv));
 
 // TODO: pass constants as parameters?
 // original: n = 512 x 4 x 4, in the testbench only do 2 instead of 512, so n = 32
 transconv tc_unit
-    (.clk(clk), .reset(reset), .start_tick(start_tick_tc), .m(14'd1), .k(14'd100), .n(14'd32),
-     .n_output_plane(10'd2), .output_plane_start(output_plane_start), .output_plane_batch_size(10'd1),
-     .output_h(7'd4), .output_w(7'd4), .input_h(7'd1), .input_w(7'd1),
-     .kernel_h(3'd4), .kernel_w(3'd4), .pad_h(3'd0), .pad_w(3'd0),
-     .stride_h(3'd1), .stride_w(3'd1), .dilation_h(3'd1), .dilation_w(3'd1),
-     .a(input_rd), .b(weight_rd), .c(output_rd0), .rv(rv0), .cv(cv1), .term4(32'd1723600), .bias(tc_bias),
+    (.clk(clk), .reset(reset), .start_tick(start_tick_tc), .m(m), .k(k), .n(n),
+     .n_output_plane(n_output_plane), .output_plane_start(output_plane_start),
+     .output_plane_batch_size(output_plane_batch_size),
+     .output_h(output_h), .output_w(output_w), .input_h(input_h), .input_w(input_w),
+     .kernel_h(kernel_h), .kernel_w(kernel_w), .pad_h(pad_h), .pad_w(pad_w),
+     .stride_h(stride_h), .stride_w(stride_w), .dilation_h(dilation_h), .dilation_w(dilation_w),
+     .a(input_rd), .b(weight_rd), .c(output_rd0), .rv(rv0), .cv(cv1), .term4(term4), .bias(tc_bias),
      .a_rd_addr(addr_input_rd_tc), .b_rd_addr(addr_weight_rd), .c_rw_addr(addr_output_rw_tc),
      .addr_rv(addr_rv0), .addr_cv(addr_cv1), .addr_bias(addr_tc_bias),
      .c_out(output_wr_tc), .c_wr_en(we_output_tc), .done_tick(done_tick_tc));
 
 dq_bn_relu_q dq_bn_relu_q_unit
-    (.clk(clk), .start_tick(start_tick_bn_relu), .sa_f(32'h3cebbba5), .sb_f(32'h3b8a47ed), .in(output_rd1),
-     .input_total_size(16'd32),
-     .input_plane_size(16'd16),
+    (.clk(clk), .start_tick(start_tick_bn_relu), .sa_f(sa_f), .sb_f(sb_f), .in(output_rd1),
+     .input_total_size(input_total_size),
+     .input_plane_size(input_plane_size),
      .out32(output_wr_bn_relu),
      .out8(input_wr),
      .addr_output_rd(addr_output_rd_bn_relu),
@@ -283,6 +310,7 @@ begin
                 start_tick_bn_relu <= 1'b0;
                 if (done_tick_bn_relu)
                     begin
+                        layer <= layer + 3;
                         state <= idle;
                     end
             end
